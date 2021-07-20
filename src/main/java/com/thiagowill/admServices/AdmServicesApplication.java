7bad16cca5b4a69e27e55136fbc80f5f6b8d6389
ThiagoWill19 <thiagopompeu19@gmail.com>
2021-07-20 17:17:57 -0300

    Start project

---
+++ src/main/java/com/thiagowill/admServices/AdmServicesApplication.java
@@ -0,0 +1,13 @@
+package com.thiagowill.admServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagowill/admServices/AdmServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagowill.admServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
